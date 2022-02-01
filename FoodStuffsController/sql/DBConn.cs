17bf414f758dd1547123eae96ec95d460adb4ea4
ATBirch <u1761249@unimail.hud.ac.uk>
2022-02-01 15:04:46 +0000

    Began implementation on reading JSON values for the database connections.

---
+++ FoodStuffsController/sql/DBConn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace FoodStuffsController.sql
+{
+    class DBConn
+    {
+
+        private string hostname { get; }
+        private string database { get; }
+        private string username { get; }
+        private string password { get; }
+
+        DBConn()
+        {
+            string jsonFile = "DBConfig.json";
+
+        }
+    }
+}
